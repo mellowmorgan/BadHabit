57400a974b2c016ea5aa3f248a20ad70c54d550d
mellowmorgan <morgan.waites@gmail.com>
2021-09-16 00:18:04 -0700

    first commit

--- app/src/main/java/com/example/badhabit/MainActivity.java
+++ app/src/main/java/com/example/badhabit/MainActivity.java
@@ -1,3 +1,8 @@
+//Dashboard for logged in user
+//displaying Welcome, User Name, date and list of trackers clickable, settings button in corner
+//trackers clickable to TrackerPageActivity
+//settings clickable to SettingsActivity
+
 package com.example.badhabit;
 
 import androidx.appcompat.app.AppCompatActivity;
