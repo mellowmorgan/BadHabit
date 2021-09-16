a59e0fa627668f16693c15f89f95e0e9ccfe8ada
mellowmorgan <morgan.waites@gmail.com>
2021-09-15 18:13:13 -0700

    first push of app in dev

---
+++ app/src/main/java/com/example/badhabit/CalendarEntryActivity.java
@@ -0,0 +1,18 @@
+package com.example.badhabit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class CalendarEntryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calendar_entry);
+    }
+    public void viewCalendar(View v){
+
+    }
+}
