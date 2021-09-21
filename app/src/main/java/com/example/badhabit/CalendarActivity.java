156ef5d8c1000c6cf63eaa0198f6cff1839990d8
mellowmorgan <morgan.waites@gmail.com>
2021-09-21 14:42:41 -0700

    calendar and day selector working

--- app/src/main/java/com/example/badhabit/CalendarActivity.java
+++ app/src/main/java/com/example/badhabit/CalendarActivity.java
@@ -1,58 +1,56 @@
 package com.example.badhabit;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.cardview.widget.CardView;
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.CalendarView;
 import android.widget.TextView;
 
 public class CalendarActivity extends AppCompatActivity {
-    // Define the variable of CalendarView type
-    // and TextView type;
-    CalendarView calendar;
-    TextView date_view;
+
+    private static final String TAG = "CalendarActivity";
+
+    private CalendarView mCalendarView;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_calendar);
+        setContentView(R.layout.calendar_layout);
 
-        // By ID we can use each component
-        // which id is assign in xml file
-        // use findViewById() to get the
-        // CalendarView and TextView
-        calendar = (CalendarView)
-                findViewById(R.id.calendar);
-        date_view = (TextView)
-                findViewById(R.id.date_view);
+        Intent i = getIntent();
+        String howDaySelected = i.getStringExtra("howDay");
+        String dateSelected = i.getStringExtra("date");
+        hasSavedDay(howDaySelected, dateSelected);
 
-        // Add Listener in calendar
-        calendar.setOnDateChangeListener(
-                        new CalendarView
-                                .OnDateChangeListener() {
-                            @Override
+        mCalendarView = (CalendarView) findViewById(R.id.calendarView);
 
-                            // In this Listener have one method
-                            // and in this method we will
-                            // get the value of DAYS, MONTH, YEARS
-                            public void onSelectedDayChange(
-                                    CalendarView view,
-                                    int year,
-                                    int month,
-                                    int dayOfMonth)
-                            {
+        mCalendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(CalendarView calendarView, int i, int i1, int i2) {
+                String date = (i1 + 1) + "/" + i2 + "/" + i;
+                Log.d(TAG, "onSelectedDayChange: mm/dd/yyyy: " + date);
+                Intent intent = new Intent(CalendarActivity.this, CalendarEntryPopUpActivity.class);
+                intent.putExtra("date", date);
+                startActivity(intent);
 
-                                // Store the value of date with
-                                // format in String type Variable
-                                // Add 1 in month because month
-                                // index is start with 0
-                                String Date
-                                        = dayOfMonth + "-"
-                                        + (month + 1) + "-" + year;
+            }
 
-                                // set this date in TextView for Display
-                                date_view.setText(Date);
-                            }
-                        });
+        });
+        //if intent is there trigger hasSavedDay, make calendar show saved days? might need to put at to
+
+
+    }
+    public void hasSavedDay(String d1, String d2){
+        TextView t = findViewById(R.id.text_view);
+
+        if ("good".equals(d1)){
+            t.setText(d1 + " " + d2);
+        }
+        if ("bad".equals(d1)){
+            t.setText(d1 + " " + d2);
+
+        }
+
     }
 }
--- app/src/main/java/com/example/badhabit/CalendarEntryPopUpActivity.java
+++ app/src/main/java/com/example/badhabit/CalendarEntryPopUpActivity.java
@@ -2,13 +2,80 @@ package com.example.badhabit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class CalendarEntryPopUpActivity extends AppCompatActivity {
-
+public class CalendarEntryPopUpActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener {
+    private Switch aSwitch;
+    private Switch bSwitch;
+    private String howDaySelector = "nothing";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar_entry_pop_up);
+        Intent i = getIntent();
+        String s = i.getStringExtra("date");
+        TextView t = findViewById(R.id.textView2);
+        t.setText(s);
+        aSwitch = findViewById(R.id.switch_a);
+        bSwitch = findViewById(R.id.switch_b);
+        aSwitch.setOnCheckedChangeListener(this);
+        bSwitch.setOnCheckedChangeListener(this);
+
+
+    }
+    @Override
+    public void onCheckedChanged(CompoundButton btn, boolean isChecked){
+        if (btn==aSwitch){
+            if (isChecked){
+                showMessage("Switch Good Day");
+                bSwitch.setClickable(false);
+                howDaySelector = "good";
+            }
+        }
+        if (btn==bSwitch){
+            if (isChecked){
+                showMessage("Switch Bad Day");
+                aSwitch.setClickable(false);
+                howDaySelector = "bad";
+
+            }}
+        if (aSwitch.isChecked()==false & bSwitch.isChecked()==false){
+            bSwitch.setClickable(true);
+            aSwitch.setClickable(true);
+            howDaySelector="nothing";
+        }
+
+
+    }
+
+    public void showMessage(String message){
+            Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+        }
+
+    public void onSave(View v){
+        Intent intent = getIntent();
+        String s = intent.getStringExtra("date");
+        Intent i = new Intent(this, CalendarActivity.class);
+        i.putExtra("howDay", howDaySelector);
+        i.putExtra("date", s);
+        startActivity(i);
+
+    }
+    public void onCancel(View v){
+        Intent i = new Intent(this, CalendarActivity.class);
+        startActivity(i);
+
     }
 }
+
+
+
+
+
